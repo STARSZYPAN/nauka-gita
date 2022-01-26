6b5cb80711c93965d5ba72e125fe05a5bab8f58b
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-01-26 20:14:38 +0100

    zapis kodu.

---
+++ nauka.py
@@ -0,0 +1,10 @@
+lista={
+    'piekarnia': ['bulki','chleb','paczek'],
+    'warzywniak': ['buraki','facola','ziemniaki']
+
+}
+lista_dict=dict(lista)
+print('Lista zakupow')
+for sklep,rzeczy in lista_dict.items():
+    print('ide do',(sklep.upper()),'kupuje',(rzeczy))
+print('razem kupilem',len(rzeczy)*2,'rzeczy')
