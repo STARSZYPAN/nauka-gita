3d329eea15185297f8264fc33b2653b0f8a06f5f
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-01-26 20:28:44 +0100

    zmiana tekstu

--- nauka.py
+++ nauka.py
@@ -1,12 +1,3 @@
-lista={
-    'piekarnia': ['bulki','chleb','paczek'],
-    'warzywniak': ['buraki','facola','ziemniaki']
 
-}
-lista_dict=dict(lista)
-print('Lista zakupow')
-for sklep,rzeczy in lista_dict.items():
-    print('ide do',(sklep.upper()),'kupuje',(rzeczy))
-print('razem kupilem',len(rzeczy)*2,'rzeczy')
-print("pozdrowienia dla mentora")
+print("pozdrowienia dla mentora,dzięki za wsprcie")
 
